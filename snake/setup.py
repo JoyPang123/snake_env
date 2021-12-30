3d5c8ffc70942b2ca64f145e56647594f46ae5c7
Justin <justin900429@gmail.com>
2021-12-31 00:29:15 +0800

    Update requirements for snake environment

--- snake/setup.py
+++ snake/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="snake",
     version="0.0.1",
-    install_requires=["gym", "numpy", "pygame"]
+    install_requires=["gym", "numpy", "pygame", "pyglet"]
 )
